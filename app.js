f0e538477c092c54820b04f375036b91f928509b
Kev-rossi <kr.bat@outlook.fr>
2022-02-02 15:54:14 +0100

    created folder : app, config and add .env

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from 'cors';
+import { setupRoutes } from "../BorrelStoryCards/routes/router.js";
+
+const app = express();
+const port= 5000;
+
+app.use(cors('*'));
+app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+
+
+setupRoutes(app);
+
+app.listen(port, () => console.log('Sever is running on port'  + port));
